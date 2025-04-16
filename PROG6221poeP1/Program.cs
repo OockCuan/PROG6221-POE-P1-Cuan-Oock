b66a2daac49101b37508b74635215ea81c895c58
OockCuan <st10439216@imconnect.edu.za>
2025-04-16 16:50:26 +0200

    First commit
    Core functionality added with some UI elements

---
+++ PROG6221poeP1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PROG6221poeP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chatbot chat = new Chatbot();
+            chat.greeting();
+            string response  = Console.ReadLine();
+
+
+
+            while (!(response.Equals("Stop") | response.Equals("stop"))) 
+            {
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(chat.keyWords(response));
+                Console.WriteLine(chat.questionWords(response));
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.White;
+                response = Console.ReadLine();
+                
+            }
+            
+            
+            //Console.ReadKey();
+        }
+    }
+}
