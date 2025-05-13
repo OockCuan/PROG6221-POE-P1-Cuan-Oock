ef64a7feaa24080d46a06c610ffca244dd7e4d78
OockCuan <st10439216@imconnect.edu.za>
2025-05-13 16:22:39 +0200

    POE p2 first commit

--- ProgPoeP1/obj/Debug/net8.0/ProgPoeP1.AssemblyInfo.cs
+++ ProgPoeP1/obj/Debug/net8.0/ProgPoeP1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgPoeP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97548843e2a818ce5cde19ae0711cc05bb29f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d477f1c15025e65f535bf07abb9566a630cb4a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgPoeP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgPoeP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
